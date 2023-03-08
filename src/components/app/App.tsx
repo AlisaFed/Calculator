5a39b4cb5f293da184466b83a080480fc7b0430a
AlisaFed <alisa.fed.91@gmail.com>
2023-03-08 20:00:55 +0300

    add main box and switch buttons

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,21 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <div className='calc-box'>
+        <div className='calc-box-buttons'>
+          <button className='switch runtime'>
+            <image className='switch-img eye'></image>
+            <p>Runtime</p>
+          </button>
+          <button className='switch constructor active'>
+          <image className='switch-img selector'></image>
+            <p>Constructor</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
